381687aba8308e30cab78db08bb42e4afacc01ba
Robert Garrigos <robert@garrigos.cat>
2026-01-08 22:02:42 +0100

    Update core/update.php
    Co-authored-by: Robert J. Lang <robert@langorigami.com>

--- core/update.php
+++ core/update.php
@@ -223,7 +223,7 @@ function update_results_page() {
   if (settings_get('update_free_access')) {
     backdrop_set_message(t("Reminder: Don't forget to set the !update value in your !settings file back to !false.", array(
       '!update' => "<code>\$settings['update_free_access']</code>",
-        '!settings' => '<code>settings.php</code>',
+      '!settings' => '<code>settings.php</code>',
         '!false' => '<code>FALSE</code>',
       )), 'warning');
   }
